462cd233060a55f8c06fba7091dc931b790f3e3b
Suejin Kim <kim.suejin.20@gmail.com>
2021-10-04 19:28:36 -0400

    history saving to local storage and displaying on history page

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
 const History = () => {
-    
+    const clearHistory = () => {
+        let history = []
+        localStorage.setItem("stories", JSON.stringify(history))
+        window.location.reload()
+    }
+    const storedQueries = JSON.parse(localStorage.getItem('stories'))
+
     return (
         <div>
-            History
+            <h1>Search History</h1>
+            <ul>
+                {storedQueries.map((query, index) => (
+                    <li key={index}> {query}</li>
+
+                ))}
+            </ul>
+            <button onClick={clearHistory}>Clear History</button>
+
         </div>
     )
 }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import useFetch from 'react-fetch-hook'
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 const Search = () => {
     const url = 'http://hn.algolia.com/api/v1/search_by_date?query='
     const [search, setSearch] = useState(null)
-    const [query, setQuery] = useState('null')
+    const [query, setQuery] = useState(null)
     const { isLoading, data, error } = useFetch(url + search + '&tags=story') 
     let stories = []
+    let history = JSON.parse(localStorage.getItem('stories')) || []
 
     if (isLoading) {
       return "loading..."
@@ -24,17 +32,25 @@ const Search = () => {
         event.preventDefault()
 
         setSearch(query)
+        
+        if (history.indexOf(query) == -1){
+            history.push(query)
+            localStorage.setItem('stories', JSON.stringify(history))
+        }
+        setQuery(null)
+
     }
    
     
     if (search) {
-
-        stories = data.hits
+        stories = data.hits || []
+        console.log(stories)
     }
 
     return (
         <div>
-            <div>
+            <h1> Hacker News Search App</h1>
+            <Box component="span" sx={{ display: 'inline-block', mx: '2px' }}>
                 <form>
                     <input
                         type={'text'}
@@ -43,11 +59,26 @@ const Search = () => {
                     />
                     <button type='button' onClick={handleSubmit}>Search</button>
                 </form>
-            </div>
+            </Box>
             <div>
-                {stories?.map((story) => (
-                    <p key={story.objectID}>{story.title}</p>
+                {stories.map((story) => (
+                    <Box key={story.objectID} component="span" sx={{ display: 'inline-block', mx: '2px' }}>
+                        <Card >
+                            <CardContent>
+                                <Typography variant="h5" >
+                                    {story.title}
+                                </Typography>
+                                <p>
+                                    {story.story_text}
+                                </p>
+                                <Button href={story.url}>
+                                    Click to View Story
+                                </Button>
+                            </CardContent>
+                        </Card>
+                    </Box>
                 ))}
+
             </div>
         </div>
     )
